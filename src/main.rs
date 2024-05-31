050e0354476e70258e3464d7c98cde85a36515ba
Kramer Campbell <kramer@kramerc.com>
2024-05-30 19:48:36 -0700

    Add basic audio tag parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use audiotags::Tag;
+
 fn main() {
-    println!("Hello, world!");
+    let path = std::path::Path::new("E:\\Music");
+
+    println!("Path: {:?}", path);
+    println!("Path exists: {}", path.exists());
+
+    recurse_directory(path);
+}
+
+fn read_tags(path: &std::path::Path) {
+    let tag = Tag::new().read_from_path(path);
+
+    if tag.is_ok() {
+        let tag = tag.unwrap();
+        println!("{} - {}",
+                 tag.artist().unwrap_or("Unknown"),
+                 tag.title().unwrap_or("Unknown"));
+    }
+}
+
+fn recurse_directory(path: &std::path::Path) {
+    // Enumerate over the directory recursively
+    for entry in path.read_dir().unwrap() {
+        let entry = entry.unwrap();
+        let path = entry.path();
+
+        if path.is_dir() {
+            println!("{}", path.to_str().unwrap_or(""));
+            recurse_directory(&path);
+        } else {
+            read_tags(&path);
+        }
+    }
 }
