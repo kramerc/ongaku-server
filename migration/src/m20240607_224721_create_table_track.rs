db94b531bf6a79cca5829412076e8ccf8ae84fd8
Kramer Campbell <kramer@kramerc.com>
2024-06-07 15:49:44 -0700

    Replace database operations with SeaORM

---
+++ migration/src/m20240607_224721_create_table_track.rs
@@ -0,0 +1,75 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(Track::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(Track::Id)
+                            .integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(Track::Uuid).string().not_null().unique_key())
+                    .col(ColumnDef::new(Track::Path).string().not_null())
+                    .col(ColumnDef::new(Track::Extension).string().not_null())
+                    .col(ColumnDef::new(Track::Title).string().not_null())
+                    .col(ColumnDef::new(Track::Artist).string().not_null())
+                    .col(ColumnDef::new(Track::Album).string().not_null())
+                    .col(ColumnDef::new(Track::Genre).string().not_null())
+                    .col(ColumnDef::new(Track::AlbumArtist).string().not_null())
+                    .col(ColumnDef::new(Track::Publisher).string().not_null())
+                    .col(ColumnDef::new(Track::CatalogNumber).string().not_null())
+                    .col(ColumnDef::new(Track::DurationSeconds).integer().not_null())
+                    .col(ColumnDef::new(Track::AudioBitrate).integer().not_null())
+                    .col(ColumnDef::new(Track::OverallBitrate).integer().not_null())
+                    .col(ColumnDef::new(Track::SampleRate).integer().not_null())
+                    .col(ColumnDef::new(Track::BitDepth).integer().not_null())
+                    .col(ColumnDef::new(Track::Channels).integer().not_null())
+                    .col(ColumnDef::new(Track::Tags).text().not_null())
+                    .col(ColumnDef::new(Track::Created).date_time().not_null())
+                    .col(ColumnDef::new(Track::Modified).date_time().not_null())
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(Track::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+enum Track {
+    Table,
+    Id,
+    Uuid,
+    Path,
+    Extension,
+    Title,
+    Artist,
+    Album,
+    Genre,
+    AlbumArtist,
+    Publisher,
+    CatalogNumber,
+    DurationSeconds,
+    AudioBitrate,
+    OverallBitrate,
+    SampleRate,
+    BitDepth,
+    Channels,
+    Tags,
+    Created,
+    Modified,
+}
--- src/main.rs
+++ src/main.rs
@@ -3,54 +3,45 @@ use std::fmt::Debug;
 use std::sync::mpsc;
 use std::sync::mpsc::Sender;
 use std::thread;
+use std::time::Duration;
 
 use lofty::prelude::*;
 use lofty::probe::Probe;
-use log::{debug, error, info};
-use ml_progress::{progress_builder};
+use log::{debug, error};
+use ml_progress::progress_builder;
 use regex::Regex;
-use rusqlite::{Connection, params};
+use sea_orm::{ActiveModelTrait, ColumnTrait, ConnectOptions, Database, DatabaseConnection, DbErr, EntityTrait, NotSet, PaginatorTrait, QueryFilter};
+use sea_orm::ActiveValue::{Set, Unchanged};
 use uuid::Uuid;
 
-use crate::library::Track;
+use entity::prelude::Track;
+use entity::track;
+use migration::{Migrator, MigratorTrait};
 
-mod library;
 mod logger;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), DbErr> {
     logger::init().unwrap();
 
-    let connection = Connection::open("ongaku.db").unwrap();
-    let query = "CREATE TABLE IF NOT EXISTS tracks (
-        id INTEGER PRIMARY KEY,
-        uuid TEXT NOT NULL UNIQUE,
-        path TEXT NOT NULL,
-        extension TEXT NOT NULL,
-        title TEXT NOT NULL,
-        artist TEXT NOT NULL,
-        album TEXT NOT NULL,
-        genre TEXT NOT NULL,
-        album_artist TEXT NOT NULL,
-        publisher TEXT NOT NULL,
-        catalog_number TEXT NOT NULL,
-        duration_seconds INTEGER NOT NULL,
-        audio_bitrate INTEGER NOT NULL,
-        overall_bitrate INTEGER NOT NULL,
-        sample_rate INTEGER NOT NULL,
-        bit_depth INTEGER NOT NULL,
-        channels INTEGER NOT NULL,
-        tags TEXT NOT NULL,
-        created TEXT NOT NULL,
-        modified TEXT NOT NULL
-    )";
-    connection.execute(query, ()).expect("Failed to create table");
+    let mut opt = ConnectOptions::new("sqlite://ongaku.db?mode=rwc");
+    opt.max_connections(100)
+        .min_connections(5)
+        .connect_timeout(Duration::from_secs(8))
+        .acquire_timeout(Duration::from_secs(8))
+        .idle_timeout(Duration::from_secs(8))
+        .max_lifetime(Duration::from_secs(8))
+        .sqlx_logging(true)
+        .sqlx_logging_level(log::LevelFilter::Info);
+    let db: DatabaseConnection = Database::connect(opt).await?;
+    Migrator::up(&db, None).await?;
 
     let path = std::path::Path::new("E:\\Music");
 
     println!("Path: {:?}", path);
     println!("Path exists: {}", path.exists());
 
-    let modified_by_path = get_all_modified_by_path(&connection).unwrap();
+    let modified_by_path = get_all_modified_by_path(&db).await?;
     let count = count_files(path);
     let progress = progress_builder!(
         "[" percent "] " pos_group "/" total_group " " bar_fill " (" eta_hms " @ " speed "it/s)"
@@ -67,10 +58,10 @@ fn main() {
 
     for track in rx {
         // TODO: Improve performance, currently takes a while after an initial scan
-        let result = upsert_track(&track, &connection);
+        let result = upsert_track(&track, &db).await;
         match result {
             Ok(_) => {
-                debug!("Inserted/updated track: {}", track.path);
+                debug!("Inserted/updated track: {}", path.to_str().unwrap_or(""));
             },
             Err(e) => {
                 error!("Error inserting {}: {:?}", path.to_str().unwrap_or(""), e);
@@ -78,44 +69,24 @@ fn main() {
         }
     }
 
-    println!("{} tracks are in the database", count_tracks(&connection));
+    println!("{} tracks are in the database", Track::find().count(&db).await?);
+    Ok(())
 }
 
-struct PathModified {
-    path: String,
-    modified: chrono::DateTime<chrono::Utc>,
-}
-
-fn get_all_modified_by_path(connection: &Connection) -> Result<HashMap<String, chrono::DateTime<chrono::Utc>>, rusqlite::Error> {
-    let select_query = "SELECT path, modified FROM tracks";
-    let mut stmt = connection.prepare(select_query)?;
+async fn get_all_modified_by_path(db: &DatabaseConnection) -> Result<HashMap<String, chrono::DateTime<chrono::Utc>>, DbErr> {
+    let tracks: Vec<track::Model> = Track::find().all(db).await?;
 
     let mut result = HashMap::new();
-    let map = stmt.query_map([], |row| {
-        Ok(PathModified {
-            path: row.get(0)?,
-            modified: row.get(1)?
-        })
-    })?;
-    for item in map {
-        let item = item.unwrap();
-        result.insert(item.path, item.modified);
+    for track in tracks {
+        result.insert(track.path, track.modified);
     }
+
     Ok(result)
 }
 
-fn get_uuid_for_track(path: &String, connection: &Connection) -> String {
-    let select_query = "SELECT uuid FROM tracks WHERE path = ?1";
-    let mut stmt = connection.prepare(select_query).unwrap();
-
-    stmt.query_row([path.to_string()], |row| {
-        row.get(0)
-    }).unwrap_or(Uuid::new_v4().to_string())
-}
-
-fn read_tags(path: &std::path::Path) -> Result<Track, OngakuError> {
+fn read_tags(path: &std::path::Path) -> Result<track::ActiveModel, OngakuError> {
     if !path.is_file() {
-        return Err(OngakuError::NotFile());
+        return Err(OngakuError::NotFile);
     }
 
     // stat file
@@ -154,199 +125,46 @@ fn read_tags(path: &std::path::Path) -> Result<Track, OngakuError> {
         all_tags.insert(key, value);
     }
 
-    Ok(Track {
-        uuid: None,
-        path: path.to_str().unwrap_or("").to_string(),
-        extension: path.extension().unwrap_or_default().to_str().unwrap_or("").to_string(),
-        title: tag.title().as_deref().unwrap_or("").to_string(),
-        artist: tag.artist().as_deref().unwrap_or("").to_string(),
-        album: tag.album().as_deref().unwrap_or("").to_string(),
-        genre: tag.genre().as_deref().unwrap_or("").to_string(),
-        album_artist: tag.get_string(&ItemKey::AlbumArtist).unwrap_or("").to_string(),
-        publisher: tag.get_string(&ItemKey::Publisher).unwrap_or("").to_string(),
-        catalog_number: tag.get_string(&ItemKey::CatalogNumber).unwrap_or("").to_string(),
-        duration_seconds: duration.as_secs(),
-        audio_bitrate: properties.audio_bitrate().unwrap_or(0),
-        overall_bitrate: properties.overall_bitrate().unwrap_or(0),
-        sample_rate: properties.sample_rate().unwrap_or(0),
-        bit_depth: properties.bit_depth().unwrap_or(0),
-        channels: properties.channels().unwrap_or(0),
-        tags: all_tags,
-        created,
-        modified,
+    Ok(track::ActiveModel {
+        id: NotSet,
+        uuid: NotSet,
+        path: Set(path.to_str().unwrap_or("").to_string()),
+        extension: Set(path.extension().unwrap_or_default().to_str().unwrap_or("").to_string()),
+        title: Set(tag.title().as_deref().unwrap_or("").to_string()),
+        artist: Set(tag.artist().as_deref().unwrap_or("").to_string()),
+        album: Set(tag.album().as_deref().unwrap_or("").to_string()),
+        genre: Set(tag.genre().as_deref().unwrap_or("").to_string()),
+        album_artist: Set(tag.get_string(&ItemKey::AlbumArtist).unwrap_or("").to_string()),
+        publisher: Set(tag.get_string(&ItemKey::Publisher).unwrap_or("").to_string()),
+        catalog_number: Set(tag.get_string(&ItemKey::CatalogNumber).unwrap_or("").to_string()),
+        duration_seconds: Set(duration.as_secs() as i32),
+        audio_bitrate: Set(properties.audio_bitrate().unwrap_or(0) as i32),
+        overall_bitrate: Set(properties.overall_bitrate().unwrap_or(0) as i32),
+        sample_rate: Set(properties.sample_rate().unwrap_or(0) as i32),
+        bit_depth: Set(properties.bit_depth().unwrap_or(0) as i32),
+        channels: Set(properties.channels().unwrap_or(0) as i32),
+        tags: Set(serde_json::to_string(&all_tags).unwrap()),
+        created: Set(created),
+        modified: Set(modified),
     })
 }
 
-fn insert_track(track: &Track, connection: &Connection) -> Result<(), rusqlite::Error> {
-    let uuid = get_uuid_for_track(&track.path, &connection);
-    let insert_query = "INSERT INTO tracks (
-        uuid,
-        path,
-        extension,
-        title,
-        artist,
-        album,
-        genre,
-        album_artist,
-        publisher,
-        catalog_number,
-        duration_seconds,
-        audio_bitrate,
-        overall_bitrate,
-        sample_rate,
-        bit_depth,
-        channels,
-        tags,
-        created,
-        modified
-    ) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10, ?11, ?12, ?13, ?14, ?15, ?16, ?17, ?18, ?19)";
-    connection.execute(insert_query, params![
-        &uuid,
-        &track.path,
-        &track.extension,
-        &track.title,
-        &track.artist,
-        &track.album,
-        &track.genre,
-        &track.album_artist,
-        &track.publisher,
-        &track.catalog_number,
-        &track.duration_seconds,
-        &track.audio_bitrate,
-        &track.overall_bitrate,
-        &track.sample_rate,
-        &track.bit_depth,
-        &track.channels,
-        &serde_json::to_string(&track.tags).unwrap(),
-        &track.created,
-        &track.modified,
-    ]).expect("Failed to insert");
-
-    debug!("Inserted track: {}", uuid);
-    Ok(())
-}
-
-fn update_track(track: &Track, connection: &Connection) -> Result<(), rusqlite::Error> {
-    let update_query = "UPDATE tracks SET
-        title = ?1,
-        artist = ?2,
-        album = ?3,
-        genre = ?4,
-        album_artist = ?5,
-        publisher = ?6,
-        catalog_number = ?7,
-        duration_seconds = ?8,
-        audio_bitrate = ?9,
-        overall_bitrate = ?10,
-        sample_rate = ?11,
-        bit_depth = ?12,
-        channels = ?13,
-        tags = ?14,
-        created = ?15,
-        modified = ?16
-        WHERE path = ?17";
-    connection.execute(update_query, params![
-        &track.title,
-        &track.artist,
-        &track.album,
-        &track.genre,
-        &track.album_artist,
-        &track.publisher,
-        &track.catalog_number,
-        &track.duration_seconds,
-        &track.audio_bitrate,
-        &track.overall_bitrate,
-        &track.sample_rate,
-        &track.bit_depth,
-        &track.channels,
-        &serde_json::to_string(&track.tags).unwrap(),
-        &track.created,
-        &track.modified,
-        &track.path,
-    ])?;
-
-    debug!("Updated track: {}", track.path);
-    Ok(())
-}
-
-fn upsert_track(track: &Track, connection: &Connection) -> Result<(), rusqlite::Error> {
-    let select_query = "SELECT COUNT(*) FROM tracks WHERE path = ?1";
-    let mut stmt = connection.prepare(select_query)?;
-    let count: i32 = stmt.query_row([&track.path], |row| {
-        row.get(0)
-    })?;
-
-    if count == 0 {
-        insert_track(&track, &connection)
+async fn upsert_track(track: &track::ActiveModel, db: &DatabaseConnection) -> Result<track::Model, DbErr> {
+    let existing_track = Track::find().filter(track::Column::Path.eq(track.path.clone().unwrap())).one(db).await?;
+    if existing_track.is_none() {
+        let mut track = track.clone();
+        track.uuid = Set(Uuid::new_v4().to_string());
+        track.insert(db).await
     } else {
-        update_track(&track, &connection)
+        let existing_track = existing_track.unwrap();
+        let mut track = track.clone();
+        track.id = Unchanged(existing_track.id);
+        track.uuid = Unchanged(existing_track.uuid);
+        track.update(db).await
     }
 }
 
-fn get_track(path: &std::path::Path, connection: &Connection) -> Result<Option<Track>, rusqlite::Error> {
-    let select_query = "SELECT
-            uuid,
-            title,
-            artist,
-            album,
-            genre,
-            album_artist,
-            publisher,
-            catalog_number,
-            duration_seconds,
-            audio_bitrate,
-            overall_bitrate,
-            sample_rate,
-            bit_depth,
-            channels,
-            path,
-            extension,
-            tags,
-            created,
-            modified
-        FROM tracks WHERE path = ?1 LIMIT 1";
-    let mut stmt = connection.prepare(select_query)?;
-    let tracks = stmt.query_map([path.to_str()], |row| {
-        let tags_json: String = row.get(16)?;
-        let tags: HashMap<String, String> = serde_json::from_str(tags_json.as_str()).unwrap();
-
-        Ok(Track {
-            uuid: row.get(0)?,
-            title: row.get(1)?,
-            artist: row.get(2)?,
-            album: row.get(3)?,
-            genre: row.get(4)?,
-            album_artist: row.get(5)?,
-            publisher: row.get(6)?,
-            catalog_number: row.get(7)?,
-            duration_seconds: row.get(8)?,
-            audio_bitrate: row.get(9)?,
-            overall_bitrate: row.get(10)?,
-            sample_rate: row.get(11)?,
-            bit_depth: row.get(12)?,
-            channels: row.get(13)?,
-            path: row.get(14)?,
-            extension: row.get(15)?,
-            created: row.get(17)?,
-            modified: row.get(18)?,
-            tags
-        })
-    })?;
-    let result = tracks.last();
-    match result {
-        Some(Ok(track)) => Ok(Some(track)),
-        _ => Ok(None)
-    }
-}
-
-fn count_tracks(connection: &Connection) -> u64 {
-    let mut stmt = connection.prepare("SELECT COUNT(*) FROM tracks").unwrap();
-    stmt.query_row([], |row| {
-        row.get(0)
-    }).unwrap()
-}
-
-fn scan_dir(path: &std::path::Path, tx: &Sender<Track>, modified_by_path: &HashMap<String, chrono::DateTime<chrono::Utc>>, progress: &ml_progress::Progress) {
+fn scan_dir(path: &std::path::Path, tx: &Sender<track::ActiveModel>, modified_by_path: &HashMap<String, chrono::DateTime<chrono::Utc>>, progress: &ml_progress::Progress) {
     for entry in path.read_dir().unwrap() {
         let entry = entry.unwrap();
         let path = entry.path();
@@ -369,7 +187,7 @@ fn scan_dir(path: &std::path::Path, tx: &Sender<Track>, modified_by_path: &HashM
                         if lofty::file::FileType::from_path(&path).is_some() {
                             error!("Error reading tags: {:?}", e)
                         }
-                        }
+                    }
                 }
             }
             progress.inc(1);
@@ -397,5 +215,5 @@ fn count_files(path: &std::path::Path) -> u64 {
 enum OngakuError {
     ReadTag(lofty::error::LoftyError),
     ReadTagNoTags,
-    NotFile(),
+    NotFile,
 }
